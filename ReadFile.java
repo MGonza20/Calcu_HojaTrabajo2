9935438b1fa9ca38db710617cf823ab502fadbd0
Estef072 <61289015+Estef072@users.noreply.github.com>
2021-02-05 23:17:35 -0600

    Add files via upload

--- ReadFile.java
+++ ReadFile.java
@@ -2,10 +2,19 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;// Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
+/**
+ * Lee el archivo
+ * @author Sayra Estefania
+ *
+ */
 public  class ReadFile{
 	
-	
-	public void read(String filename) {
+	/**
+	 * Lee el archivo
+	 * @param filename el nombre del archivo
+	 * @return el contenido
+	 */
+	public String read(String filename) {
 		try {
 		      File myObj = new File(filename);
 		      Scanner myReader = new Scanner(myObj);  
@@ -17,7 +26,7 @@ public  class ReadFile{
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
-		    } 
+		    }
+		return filename; 
 	}
 }
-
